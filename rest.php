8653e4f32896ccad4c8b06887cbd34b790c13e4e
alexandr-nazaruk <alexandr.nazaruk@gmail.com>
2025-12-19 22:26:42 +0200

    fixed device name parsing

--- rest.php
+++ rest.php
@@ -3,8 +3,6 @@ require_once __DIR__ . '/inc/common.php';
 require_once __DIR__ . '/inc/alsa.php';
 require_once __DIR__ . '/inc/sql.php';
 
-
-
 class AppleMeta {
     public $track;
     public $artist;
@@ -12,21 +10,25 @@ class AppleMeta {
 
     public function __construct() {
         $parts = explode('~~~', file_get_contents(APLMETA_FILE));
-
         if (count($parts) === 6) {
           $this->track  = $parts[0];
           $this->artist = $parts[1];
-          $this->cover  = $parts[4];
+          $this->cover  = str_replace('\/', '/', $parts[4]);
         } else {
           $this->track  = "INVALID_FORMAT";
         }
     }
 }
 
+function parseDeviceName($deviceList) {
+  $parts = explode(' ', $deviceList[2], 3);
+  if (isset($parts[2])) {
+    return $parts[2]; 
+  }
+}
+
 $dbh = sqlConnect();
 
-//$result = sqlQuery("SELECT param FROM cfg_system ", $dbh);
-//print_r($result);
 $data = [];
 $result = sysCmd('pgrep -l bluealsa-aplay');
 $btActive = strpos($result[0], 'bluealsa-aplay') !== false;
@@ -35,10 +37,10 @@ $audioout = sqlQuery("SELECT value FROM cfg_system WHERE param='audioout'", $dbh
 $source = "undefined";
 if ($aplActive == '1') {
   $data['source'] = "AirPlay";
-  $data['trackinfo'] = get_object_vars(new AppleMeta());
+  $data['metadata'] = get_object_vars(new AppleMeta());
 } else if ($btActive === true && $audioout == 'Local') {
   $data['source'] = "Bluetooth";
-  $data['device'] = sysCmd('/var/www/util/blu-control.sh -c');
+  $data['device'] = parseDeviceName(sysCmd('/var/www/util/blu-control.sh -c'));
 } else {
   $data['source'] = "Local";
 }
